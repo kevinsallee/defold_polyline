5d3e108d15df6dc31e2bc072392be00eccde5f68
builder <builder@ip-10-0-0-53.eu-west-1.compute.internal>
2017-02-21 00:15:46 +0000

    Initial commit

---
+++ main/block.go
@@ -0,0 +1,63 @@
+components {
+  id: "script"
+  component: "/main/block.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "cover"
+  type: "sprite"
+  data: "tile_set: \"/def-magic-link/sprites.atlas\"\ndefault_animation: \"magic-sphere_layer4\"\n"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.03
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "fxfactory"
+  type: "factory"
+  data: "prototype: \"/main/magic_fx.go\"\n"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/def-magic-link/sprites.atlas\"\ndefault_animation: \"orange\"\n"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
